30ad2153966b74d00f1fb12363fce2e8747eab23
Richard Morris <rwmorris.bjj@gmail.com>
2020-04-10 17:39:17 +0100

    parsing code on simple code extracts to consolidate knowledge

--- study_group_problem.rb
+++ study_group_problem.rb
@@ -5,15 +5,17 @@ def fix(number)
 end
 
 fix(a)
-p a 
+p a
 
 # On line 1, the variable a is initalised with the value of the integer 5.
-# The method fix is defined on line 3 with the parameter number.
-# on line 3, the variable number is initalized and assigned the value of the integer 7.
-# on line 7, fix is invoked and the variable a is passed to it as an arguement.
-# a's value remains at 5, as number within the method is a new variable and a different
-# object to that of a.
-# on line 8, the p method is invoked and it returns tha value of a.
-# p returns the integer 5. 
 
-# `This is an example of variables as pointers and variable initalization.
+# The method fix is defined on lines 3 - 5 with the parameter, number.
+
+# On line 7, fix is invoked and the variable a is passed to it as an arguement.
+
+# On line 5, the local variable number , within the scope of the method fix, is assigned the value of integer 7. 
+# However, this does not mutate a in the outerscope, as the outerscope does not have access to the innerscope of 
+# the method. As a result, a still references the value 5. 
+
+# On line 8, the p method is invoked and a is passed to it as an arguement. 
+#The value of a is returned. a's value is the integer 5.
