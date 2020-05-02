ef51a11135e59d0c5b310de9ac1e7b7588a5a292
richwynmorris <rwmorris.bjj@gmail.com>
2020-05-02 17:20:11 +0100

    added solution to codewars problem

--- number_zoo_patrol.rb
+++ number_zoo_patrol.rb
@@ -26,35 +26,11 @@ Rules:
                 if the pattern is complete but 1 is missing, add one
                 if the pattern if missing a number between, return the missing number.
 
-Mental Model:
-if array doesn't include 1, return 1. iterate through the array and check for any missing
-numbers in the sequence. If there's not missing numbers return the last object in the array plus 1. 
-
-Algorithm:
-    define the method find_missing_number with one parameter, numbers.
-    return 1 if numbers.include?(1) returns false
-    number to call the each method. If the element at index position 1, 
-    is equal to the the current element + 1, next.
-    else return index + 1.
-
-    return numbers.pop + 1 
-
-
 =end
 
- def find_missing_number(numbers)
-  
-  numbers.sort!
 
-	return 1 if numbers[0] != 1
-
-	numbers.each_with_index do |element, index|
-		next if numbers[index + 1] == element + 1
-			return element + 1
-	end
-
-	return numbers.pop + 1
-
+def find_missing_number(array)
+  missing_array_sum = (array.length + 1) * (array.length + 2) / 2
+  original_array_sum = array.reduce(:+)
+  missing_array_sum - original_array_sum
 end
-
-
